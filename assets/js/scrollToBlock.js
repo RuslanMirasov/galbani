f13a86cbf589587360a6d237b3e5b7a2ed78ea5a
Ruslan <info@mirasov.dev>
2025-07-31 22:01:53 +0200

    redirect problems solve on IOS

--- assets/js/scrollToBlock.js
+++ assets/js/scrollToBlock.js
@@ -1,47 +1,16 @@
-// export const scrollToBlock = (selector, offset = 0) => {
-//   const element = document.querySelector(selector);
-
-//   if (!element) {
-//     // Если элемент не найден, перенаправляем на главную с якорем
-//     const targetId = selector.replace('#', '');
-//     window.location.href = `/${targetId ? `#${targetId}` : ''}`;
-//     return;
-//   }
-
-//   const y = element.getBoundingClientRect().top + window.scrollY + offset;
-
-//   window.scrollTo({
-//     top: y,
-//     behavior: 'smooth',
-//   });
-// };
-
-// const handleScrollLinkClick = e => {
-//   e.preventDefault();
-//   const target = e.target.href.split('#')[1];
-//   scrollToBlock(`#${target}`, 0);
-// };
-
-// export const initScrollToBlock = () => {
-//   const scrollLinks = document.querySelectorAll('[data-scrollto]');
-
-//   if (scrollLinks.length > 0) {
-//     scrollLinks.forEach(link => link.addEventListener('click', handleScrollLinkClick));
-//   }
-// };
-
 export const scrollToBlock = (selector, offset = 0) => {
   const element = document.querySelector(selector);
 
   if (!element) {
-    const currentPath = window.location.pathname;
-    const pathParts = currentPath.split("/").filter(Boolean);
-    const projectRoot =
-      pathParts.length > 1 ? "/" + pathParts.slice(0, 2).join("/") : "/";
+    const targetId = selector.replace('#', '');
+    const newUrl = `/${targetId ? `#${targetId}` : ''}`;
 
-    const targetId = selector.replace("#", "");
-
-    window.location.href = `${projectRoot}/${targetId ? `#${targetId}` : ""}`;
+    if (window.location.pathname === '/' || window.location.pathname === '') {
+      window.location.hash = targetId;
+      window.location.reload(); // для iOS
+    } else {
+      window.location.assign(newUrl);
+    }
     return;
   }
 
@@ -49,13 +18,13 @@ export const scrollToBlock = (selector, offset = 0) => {
 
   window.scrollTo({
     top: y,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 };
 
-const handleScrollLinkClick = (e) => {
-  const targetHref = e.currentTarget.getAttribute("href");
-  const targetId = targetHref.split("#")[1];
+const handleScrollLinkClick = e => {
+  const targetHref = e.currentTarget.getAttribute('href');
+  const targetId = targetHref.split('#')[1];
 
   if (!targetId) return;
 
@@ -64,8 +33,6 @@ const handleScrollLinkClick = (e) => {
 };
 
 export const initScrollToBlock = () => {
-  const scrollLinks = document.querySelectorAll("[data-scrollto]");
-  scrollLinks.forEach((link) =>
-    link.addEventListener("click", handleScrollLinkClick)
-  );
+  const scrollLinks = document.querySelectorAll('[data-scrollto]');
+  scrollLinks.forEach(link => link.addEventListener('click', handleScrollLinkClick));
 };
